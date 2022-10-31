28918374fc54d9b5aac81e50ca75e8b9d44fd3ea
HYUN <84500352+HYUN-8265@users.noreply.github.com>
2022-10-31 14:49:26 +0900

    Add UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PointModule } from './point/point.module';
+import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
 import { SignModule } from './sign/sign.module';
 import { Regulate } from './entity/regulate.entity';
 import { Parents } from './entity/parents.entity';
 import { Point } from './entity/point.entity';
 import { User } from './entity/user.entity';
-import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { AdminModule } from './admin/admin.module';
       autoLoadEntities: true,
     }),
     PointModule,
+    UserModule,
     SignModule,
     AdminModule
   ],
