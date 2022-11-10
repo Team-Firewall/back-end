76f2be9b47b73f2fc50fed8eb31596f9f5dbd197
HYUN <84500352+HYUN-8265@users.noreply.github.com>
2022-11-10 12:38:21 +0900

    Edit Constructor
     - configService
    + dotenv/config

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -1,24 +1,24 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config';
 import { Client as AligoClient } from 'aligo-smartsms'
 import 'dotenv/config'
 
 @Injectable()
 export class SMS_Service {
-  private client: AligoClient
+  public client: AligoClient
+  static sendSMS: any;
 
-  constructor(configService: ConfigService) {
+  constructor() {
     this.client = new AligoClient({
-      key: configService.get<string>('KEY', process.env.SMS_KEY),
-      sender: configService.get<string>('USER_ID', process.env.SMS_SENDER),
-      user_id: configService.get<string>('SENDER', process.env.SMS_USER_ID),
+      key: (process.env.SMS_KEY),
+      sender: (process.env.SMS_SENDER),
+      user_id: (process.env.SMS_USER_ID),
     });
   }
   
-  async sendMessage(phone, message) {
+  public async sendMessage(phone ,message) {
     try {
-      return await this.client.bulkSendMessages({
+        return await this.client.bulkSendMessages({
         cnt: 1,
         msg: [{
           receiver: phone,
