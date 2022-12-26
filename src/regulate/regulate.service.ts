330bb6b455f7d9f968b648857a75aa6c5268d79b
HYUN-8265 <pumdie77@gmail.com>
2022-12-26 23:06:28 +0900

    regulate crud

--- src/regulate/regulate.service.ts
+++ src/regulate/regulate.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Regulate } from '../entity/regulate.entity';
+import { Request, Response } from "express";
 
 @Injectable()
 export class RegulateService {
@@ -14,7 +15,7 @@ export class RegulateService {
     private regulateRepository: Repository<Regulate>,
   ) {}
 
-  async FindRegulate(checked: string): Promise<Regulate[]> {
+  async FindRegulate(checked: string) {
     try {
       return await this.regulateRepository.find({
         where: { checked },
@@ -32,5 +33,51 @@ export class RegulateService {
       select: ['id','checked','division', 'regulate', 'score'],
     });
     return data;
+  }
+
+  async RegulateCreate(req: Request, res: Response) {
+    const data = req.body;
+    for(let i in data){
+      const { checked, division, regulate, score } = data[i];
+      console.log(checked, division, regulate, score);
+      await this.regulateRepository.insert({
+        checked,
+        division,
+        regulate,
+        score,
+      });
+
+    };
+    res.status(200).json({
+      success: true,
+      msg: '성공적으로 상벌점 규정을 등록하였습니다.'
+    });
+  }
+
+  async RegulateUpdate(req: Request, res: Response) {
+    const data = req.body;
+      const { id, checked, division, regulate, score } = data;
+      await this.regulateRepository.update(id, {
+        checked,
+        division,
+        regulate,
+        score,
+      });
+    res.status(200).json({
+      success: true,
+      msg: '성공적으로 상벌점 규정을 수정하였습니다.'
+    });
+  }
+
+  async RegulateDelete(req: Request, res: Response) {
+    const data = req.body;
+    for (let i in data) {
+      const { id } = data[i];
+      await this.regulateRepository.delete(id);
+    }
+    res.status(200).json({
+      success: true,
+      msg: '성공적으로 상벌점 규정을 삭제하였습니다.'
+    });
   }
 }
