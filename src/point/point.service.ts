9d55a2fb1f3e8a55d88217524b37bd18465636ee
cghsuw256 <c66182527@gmail.com>
2022-12-26 16:15:35 +0900

    FindByUserId  edit

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -78,44 +78,53 @@ export class PointService {
   }
 
   async FindByUserId(req: Request, res: Response) {
-    const userId = req.body.userId;
+    const { id } = req.body;
+    const arr = [];
+    const result = [];
+    const finalResult = [];
     const data = await this.pointRepository
       .createQueryBuilder('point')
-      .where('point.userId = :userId', { userId })
-      .select(['point', 'regulate', 'user.id', 'user.grade', 'user.classNum', 'user.number', 'user.name' ])
+      .select(['point', 'regulate', 'r.score', 'user.id', 'user.grade', 'user.classNum', 'user.number', 'user.name' ])
       .leftJoin('point.user', 'user')
-      .leftJoin('point.regulate', 'regulate')
+      .leftJoin('point.regulate', 'r')
+      .where('r.Id = point.regulateId and point.userId = :id', {id})
       .getMany();
-
-    const result = (data.map(cb => {
-      const createdDate = new Date(cb.createdAt).toISOString().split('T')[0];
-      const updatedDate = new Date(cb.updatedAt).toISOString().split('T')[0];
-      const createdTime = new Date(cb.createdAt).toTimeString().split(' ')[0];
-      const updatedTime = new Date(cb.updatedAt).toTimeString().split(' ')[0];
-
-      return({
-        id: cb.id,
-        userId: cb.user.id,
-        regulateId: cb.regulate.id,
-        grade: cb.user.grade,
-        class: cb.user.classNum,
-        number: cb.user.number,
-        name: cb.user.name,
-        checked: cb.regulate.checked,
-        division: cb.regulate.division,
-        regulate: cb.regulate.regulate,
-        score: cb.regulate.score,
-        reason: cb.reason,
-        issuer: cb.issuer,
-        createdDate: createdDate,
-        createdTime: createdTime,
-        updatedDate: updatedDate,
-        updatedTime: updatedTime,
+    for(const i in data){
+      arr[i] = data[i].regulate.score;
+      const sumNum = arr.reduce((acc, cur, i) => {
+        return result[i] = acc + cur;
+      }, 0)
+      const date = data[i].createdAt;
+      const createdDate = (date).toISOString().split('T')[0];
+      finalResult[i] = {
+        id: data[i].id,
+        userId: data[i].user.id,
+        regulateId: data[i].regulateId,
+        grade: data[i].user.grade,
+        classNum: data[i].user.classNum,
+        number: data[i].user.number,
+        name: data[i].user.name,
+        checked: data[i].regulate.checked,
+        regulate: data[i].regulate.regulate,
+        reason: data[i].reason,
+        createdAt: createdDate,
+        issuer: data[i].issuer,
+        permission: data[i].user.permission,
+        score: arr[i],
+        total: result[i]
+      }
+    }
+    if(finalResult.length !== 0){
+      res.status(200).send({
+        success:true,
+        msg:'값을 성공적으로 불러왔습니다.',
+        finalResult
       })
-    }));
-
-    if (result){
-      res.status(200).json(result);
+    }else{
+      res.status(400).send({
+        success: false,
+        msg:'해당하는 유저가 존재하지 않습니다.',
+      })
     }
   }
 
