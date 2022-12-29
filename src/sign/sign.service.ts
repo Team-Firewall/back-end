b645a3ad2f026adf47f5b5a7eb5613f774ae245a
Choi Guen Ho <c66182527@gmail.com>
2022-12-29 16:05:49 +0900

    Update sign.service.ts

--- src/sign/sign.service.ts
+++ src/sign/sign.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { sign } from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import { hash, getRandom } from '../util/text';
-import { User } from '../entity/ser.entity';
+import { User } from '../entity/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 @Injectable()
